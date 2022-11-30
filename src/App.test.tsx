d0a9713e51a5d42b50915d5a2ae9a7e6fd9fcd6b
Ashis Chaudhary <ashischaudhary959@gmail.com>
2022-11-30 23:29:54 +1100

    test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./Home";
-import { data } from "./demoData";
+import { jobData } from "./demoData";
+import { workerData } from "./demoData";
 
 test("renders learn react link", () => {
-  render(<App {...data} />);
+  render(<App jobDetails={jobData} workerDetails={workerData} />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
