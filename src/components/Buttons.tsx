829f543f0bcf962509190cef5b2e9698e61229d4
Ashis Chaudhary <ashischaudhary959@gmail.com>
2022-11-30 23:32:29 +1100

    add state to button to change color

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,15 +1,58 @@
 import { Button } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Buttons.css";
 
-const Buttons: React.FC = () => {
+interface Props {
+  workerId: string;
+  JobId: string;
+}
+
+const Buttons: React.FC<Props> = ({ workerId, JobId }) => {
+  const [jobAccepted, setJobAccepted] = useState(false);
+  const [jobRejected, setJobRejected] = useState(false);
+
+  const onRejectClickHandle = () => {
+    fetch(
+      `https://test.swipejobs.com/api/worker/${workerId}/job/${JobId}/accept`
+    )
+      .then(async (res) => {
+        setJobRejected(true);
+        setJobAccepted(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const onAcceptClickHandle = () => {
+    fetch(
+      `https://test.swipejobs.com/api/worker/${workerId}/job/${JobId}/reject`
+    )
+      .then(async (res) => {
+        setJobAccepted(true);
+        setJobRejected(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="button-container">
-      <Button variant={"outline"} className="button-reject">
-        No Thanks
+      <Button
+        variant={"outline"}
+        className={"button-reject"}
+        onClick={onRejectClickHandle}
+      >
+        {jobRejected ? "Rejected" : "No Thanks"}
       </Button>
-      <Button variant={"outlined"} className="button-accept">
-        I'll Take it
+      <Button
+        variant={"outlined"}
+        className={"button-accept"}
+        sx={{ backgroundColor: jobAccepted && "#52b818" }}
+        onClick={onAcceptClickHandle}
+      >
+        {jobAccepted ? "You took this job" : "I'll Take it"}
       </Button>
     </div>
   );
